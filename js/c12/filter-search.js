3530a57c31a2f9d74b7026a10415951cc2a67bd3
NatsumeMichiru <tianmanlinex@live.com>
2017-08-15 01:37:37 +0800

    2017/8/15 c12

---
+++ js/c12/filter-search.js
@@ -0,0 +1,40 @@
+(function() {
+
+    var $imgs = $('#gallery img'),
+        $search = $('#filter-search'),
+        cache = [];
+    
+    $imgs.each(function() {
+        cache.push({
+            element: this,
+            text: this.alt.trim().toLowerCase()
+        });
+    });
+
+    function filter() {
+        var query = this.value.trim().toLowerCase();
+        cache.forEach(function(img) {
+            var index = 0;
+
+            if (query) {
+                index = img.text.indexOf(query);
+            }
+
+            if (index == -1) {
+                img.element.style.display = 'none';
+            } else {
+                img.element.style.display = '';
+            }
+            
+        });
+    }
+
+    if ('oninput' in $search[0]) {
+        $search.on('input', filter);
+    } else {
+        $search.on('keyip', filter);
+    }
+
+
+
+}());
