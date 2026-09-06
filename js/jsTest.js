7374bc86cb4e0bff54fce756bbb8b9611c68f959
NatsumeMichiru <tianmanlinex@live.com>
2026-09-07 01:25:41 +0800

    2026-09-07 01:25:35

--- js/jsTest.js
+++ js/jsTest.js
@@ -1,47 +1,59 @@
-let p = 120;
-var q = 'string';
-console.log(p);
-console.log(q);
+// let p = 120;
+// var q = 'string';
+// console.log(p);
+// console.log(q);
 
-var func = function() {
-    var foo = 1;
-    var bar = 2;
-    var sum = foo + bar;
-    return sum;
-}
-var a = func();
-console.log(func);
+// var func = function() {
+//     var foo = 1;
+//     var bar = 2;
+//     var sum = foo + bar;
+//     return sum;
+// }
+// var a = func();
+// console.log(func);
 
-function func1 () {
-    return "func1";
-}
+// function func1 () {
+//     return "func1";
+// }
 
 
-var func2 = function () {
-    return "func2";
-}
+// var func2 = function () {
+//     return "func2";
+// }
 
-var output = func2;
-console.log(func1());
+// var output = func2;
+// console.log(func1());
 
-var array1 = [1, 2];
-console.log(array1[1]);
+// var array1 = [1, 2];
+// console.log(array1[1]);
 
-var memorizer = function (memo, formula) {
-    var recur = function (n) {
-        var result = memo[n];
-        if (typeof result !== 'number') {
-            result = formula (recur, n);
-            memo[n] = result;
-        }
-        return result;
+// var memorizer = function (memo, formula) {
+//     var recur = function (n) {
+//         var result = memo[n];
+//         if (typeof result !== 'number') {
+//             result = formula (recur, n);
+//             memo[n] = result;
+//         }
+//         return result;
+//     };
+//     return recur;
+// };
+
+// var factorial = memorizer([1,1], function (a, n) {
+//     return n * a (n - 1);
+// });
+// var test = factorial(5);
+// console.log(test);
+// console.log(factorial);
+
+var fn;
+
+(function(i) {
+    fn = function() {
+        console.log(i);
     };
-    return recur;
-};
 
-var factorial = memorizer([1,1], function (a, n) {
-    return n * a (n - 1);
-});
-var test = factorial(5);
-console.log(test);
-console.log(factorial);
+    i = 100;
+})(0);
+
+fn();
